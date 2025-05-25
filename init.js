484eedb8bc2b9f66ed7b84b06c339d32e5b27150
Liam Siira <liam@siira.io>
2025-05-24 20:27:49 -0400

    Major update/cleanup\; now properly implements setting checks, implements whitespace cleaning in all file types, and better cursor tracking, to include after undo actions.

--- init.js
+++ init.js
@@ -1,38 +1,34 @@
-//////////////////////////////////////////////////////////////////////////////80
+//////////////////////////////////////////////////////////////////////80////////////////////80
 // Atheos-Beautify: Beautify PHP, CSS, JS & HTML for the Atheos IDE
-//////////////////////////////////////////////////////////////////////////////80
+//////////////////////////////////////////////////////////////////////80////////////////////80
 // Copyright (c) 2020 Liam Siira (liam@siira.io), distributed as-is and without
-// warranty under the MIT License. See [root]/docs/LICENSE.md for more.
+// warranty under the MIT License. See [root]/license.md for more.
 // This information must remain intact.
-//////////////////////////////////////////////////////////////////////////////80
+//////////////////////////////////////////////////////////////////////80////////////////////80
 // Copyright (c) 2015 Andr3as
 // Source: https://github.com/Andr3as/Codiad-Beautify
-//////////////////////////////////////////////////////////////////////////////80
+//////////////////////////////////////////////////////////////////////80////////////////////80
 
 
 (function(global) {
+	'use strict';
 
-	var atheos = global.atheos;
-
-	var self = null;
-
-	carbon.subscribe('system.loadExtra', () => atheos.Beautify.init());
-
-	atheos.Beautify = {
+	const self = {
 
 		path: atheos.path + 'plugins/Beautify/',
-
+		
 		beautifyPhp: null,
-		lines: 0,
-		row: 0,
 
-		autoBeautify: {
+		autoBeautifyExtensions: {
 			css: false,
 			html: false,
 			js: false,
 			json: false,
 			php: false
 		},
+
+		guessCursorPosition: true,
+		cleanWhitespace: true,
 
 		settings: {
 			indent_size: 1,
@@ -50,13 +46,14 @@
 			eval_code: false,
 			unescape_strings: false,
 			wrap_line_length: 0
-
 		},
 
 		types: ["html", "htm", "js", "json", "scss", "css", "twig", "php", "hbs", "handlebars"],
 
+		//////////////////////////////////////////////////////////////////////80
+		// Initalized
+		//////////////////////////////////////////////////////////////////////80
 		init: function() {
-			self = this;
 			//Load libs
 			atheos.common.loadScript(self.path + "libs/beautify-css.js");
 			atheos.common.loadScript(self.path + "libs/beautify-html.js");
@@ -80,65 +77,22 @@
 					}
 				});
 			});
-			carbon.subscribe('active.save', function(path) {
-				path = path || atheos.active.getPath();
-				var ext = pathinfo(path).extension;
 
-				if (self.types.includes(ext) && self.autoBeautifyEnabled(ext)) {
-					var content = atheos.editor.getContent();
-					self.lines = self.getLines();
-					self.row = atheos.editor.getActive().getCursorPosition().row;
-					content = self.beautifyContent(path, content);
-					if (typeof(content) !== 'string') {
-						return true;
-					}
-					atheos.editor.setContent(content);
-					self.guessCursorPosition();
-				}
-			});
+			carbon.subscribe('active.save', self.autoBeautify);
 
-			carbon.subscribe('settings.loaded, settings.save', function() {
+			carbon.subscribe('settings.loaded, settings.saved', function() {
+				self.guessCursorPosition = storage('beautify.guessCursorPosition') || true;
+				self.cleanWhitespace = storage('beautify.cleanWhitespace') || false;
 				self.autoBeautify.css = storage('beautify.css') || false;
 				self.autoBeautify.html = storage('beautify.html') || false;
 				self.autoBeautify.js = storage('beautify.js') || false;
 				self.autoBeautify.json = storage('beautify.json') || false;
 				self.autoBeautify.php = storage('beautify.php') || false;
 
-				self.settings.brace_style = storage('beautify.brace_style') || 'collapse';
 			});
 		},
 
-		//////////////////////////////////////////////////////////
-		//
-		//  Get number of lines of current document
-		//
-		//  Parameters
-		//
-		//  content - {string} - (optional) Content of current document
-		//
-		//////////////////////////////////////////////////////////
-		getLines: function(content) {
-			content = content || atheos.editor.getContent();
-			return (content.match(/\n/g) || []).length + 1;
-		},
-
-		//////////////////////////////////////////////////////////
-		//
-		//  Guess the cursor position after beautifying content
-		//
-		//////////////////////////////////////////////////////////
-		guessCursorPosition: function() {
-			var newLines = this.getLines();
-			var factor = newLines / this.lines;
-			var newRow = Math.floor(factor * this.row);
-			atheos.editor.getActive().clearSelection();
-			atheos.editor.getActive().moveCursorToPosition({
-				"row": newRow,
-				"column": 0
-			});
-		},
-
-		//////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		//
 		//  Beautify content
 		//
@@ -148,15 +102,13 @@
 		//  content - {string} - Content to beautify
 		//  settings - {object} - Settings for beautify
 		//
-		//////////////////////////////////////////////////////////
-		beautifyContent: function(path, content, settings) {
+		//////////////////////////////////////////////////////////////////////80
+		beautifyContent: function(ext, content, settings) {
 			self.checkBeautifySettings();
 
 			if (typeof(settings) == 'undefined') {
 				settings = self.settings;
 			}
-
-			var ext = pathinfo(path).extension;
 
 			if (ext === "html" || ext === "htm" || ext === "hbs" || ext === "handlebars") {
 				return html_beautify(content, settings);
@@ -165,82 +117,155 @@
 			} else if (ext === "js" || ext === "json") {
 				return js_beautify(content, settings);
 			} else if (ext === "php" || ext === "twig") {
-				self.beautifyPhp.beautify(atheos.editor.getActive().getSession());
-				return true;
+				return self.beautifyPhpString(content);
+			} else if (self.cleanWhitespace) {
+				return content.replace(/[ \t]+$/gm, '');
 			} else {
-				return false;
+			    return false;
 			}
 		},
 
-		//////////////////////////////////////////////////////////
-		//
+		//////////////////////////////////////////////////////////////////////80
 		//  Beautify command to handle hotkey
-		//
-		//////////////////////////////////////////////////////////
-		beautify: function() {
-			var settings = self.settings;
-			var path = atheos.active.getPath();
-			var editor = atheos.editor.getActive();
-			var session = editor.getSession();
-			var selText = atheos.editor.getSelectedText();
-			var range = editor.selection.getRange();
-			var fn = function(range, text) {
-				if (typeof(text) == 'undefined') {
-					settings.indent_level = "keep";
-					range.start.column = 0;
-					text = session.getTextRange(range);
-				}
-				text = self.beautifyContent(path, text, settings);
-				if (typeof(text) == 'string') {
-					session.replace(range, text);
-				}
-			};
-			if (selText !== "") {
-				if (editor.selection.inMultiSelectMode) {
-					var multiRanges = editor.selection.getAllRanges();
-					for (var i = 0; i < multiRanges.length; i++) {
-						fn(multiRanges[i]);
-					}
-				} else {
-					//Single selection
-					fn(range);
-				}
-			} else {
-				self.row = atheos.editor.getActive().getCursorPosition().row;
-				self.lines = this.getLines();
-				var content = atheos.editor.getContent();
-				range = editor.selectAll() || editor.selection.getRange();
-				fn(range, content);
+		//////////////////////////////////////////////////////////////////////80
+		beautify: function(path) {
+			var localSettings = self.settings;
+			path = path || atheos.active.getPath();
 
-				self.guessCursorPosition();
+			var session = atheos.active.getSession(path);
+			var selection = session.selection;
+
+			var activePath = atheos.active.getPath();
+			var ext = pathinfo(path).extension;
+
+			let oldCursor = (path === activePath) ? extend(selection.cursor) : false;
+			let oldIndex = (path === activePath) ? session.getDocument().positionToIndex(oldCursor) : false;
+			
+			if (selection.rangeCount == 0) {
+				selection.selectAll();
+			} else {
+				localSettings.indent_level = "keep";
+			}
+
+// 			var selectionRange = selection.getAllRanges();
+			var selectedRange = selection.getRange();
+			var oldContent = session.getTextRange(selectedRange);
+
+			// Temporarily disabling multi-selection beaufication due to range changes.
+			// for (var i = 0; i < selectionRanges.length; i++) {
+			// range.start.column = 0;
+
+			var newContent = self.beautifyContent(ext, oldContent);
+			if (typeof(newContent) !== 'string' || oldContent === newContent) return false;
+			session.replace(selectedRange, newContent);
+			selection.clearSelection();
+
+			if (path === activePath) {
+				// Save the old cursor position to the undoStack
+				const undoStack = session.getUndoManager().$undoStack;
+				undoStack[undoStack.length - 1].cursor = oldCursor;
+
+				if (self.guessCursorPosition) {
+					//  Guess the cursor position after beautifying content
+					let newIndex = self.findCursorIndex(oldContent, newContent, oldIndex);
+					let newCursor = session.getDocument().indexToPosition(newIndex);
+				// 	log(`Guessing cursor moved to ${newIndex} from ${oldIndex}`);
+					selection.moveCursorToPosition(newCursor);
+				}
 			}
 		},
 
-		//////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		//  Check the autoBeautify settings for given extension
-		//////////////////////////////////////////////////////////
-		autoBeautifyEnabled: function(ext) {
+		//////////////////////////////////////////////////////////////////////80
+		autoBeautify: function(path) {
+			if (!self.autoBeautifyEnabled) return;
+
+			path = path || atheos.active.getPath();
+			var ext = pathinfo(path).extension;
 			ext = 'htm' ? 'html' : ext;
 			ext = 'scss' ? 'css' : ext;
 
-			return self.autoBeautify[ext];
+			if (self.autoBeautifyExtensions[ext]) {
+				self.beautifyPath(path);
+			}
 		},
 
-		//////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		//  Check settings for beautify
-		//////////////////////////////////////////////////////////
+		//////////////////////////////////////////////////////////////////////80
 		checkBeautifySettings: function() {
-			var char = "";
-			var tab = 1;
-			if (atheos.editor.settings.softTabs) {
-				char = " ";
-				tab = 4;
-			} else {
-				char = "\t";
-				tab = 1;
+			let softTabs = atheos.editor.settings.softTabs;
+			self.settings.indent_char = softTabs ? " " : "\t";
+			self.settings.indent_size = softTabs ? 4 : 1;
+		},
+
+		//////////////////////////////////////////////////////////////////////80
+		//  Find new Cursor Positions
+		//////////////////////////////////////////////////////////////////////80
+		findCursorIndex: function(oldContent, newContent, oldIndex) {
+			const isWhitespace = c => /\s/.test(c);
+
+			// Step 1: Walk oldContent to count non-whitespace up to oldIndex
+			let offset = 0;
+			for (let i = 0; i < oldIndex; i++) {
+				if (!isWhitespace(oldContent[i])) offset++;
 			}
-			self.settings.indent_char = char;
-			self.settings.indent_size = tab;
+
+			// Step 2: Walk newContent to find the same offset
+			let count = 0,
+				i = 0;
+			while (i < newContent.length && count < offset) {
+				if (!isWhitespace(newContent[i])) count++;
+				i++;
+			}
+
+			return i; // new cursor index
+		},
+
+		beautifyPhpString: function(content) {
+			const EditSession = ace.require("ace/edit_session").EditSession;
+			const PhpMode = ace.require("ace/mode/php").Mode;
+
+			let session = new EditSession(content);
+			session.setMode(new PhpMode());
+
+			self.beautifyPhp.beautify(session);
+
+			return session.getValue();
 		}
 	};
-})(this);
+
+	carbon.subscribe('system.loadExtra', () => self.init());
+	atheos.beautify = self;
+
+
+	// One-time setup: extend UndoManager to track cursor positions
+	(function patchUndoManager() {
+		const UndoManager = ace.require("ace/undomanager").UndoManager;
+		log('Patching UndoManager to track cursor positions during beautification');
+
+		if (!UndoManager.prototype._patchedForCursor) {
+			const originalUndo = UndoManager.prototype.undo;
+
+			UndoManager.prototype.undo = function(...args) {
+				const entry = this.$undoStack[this.$undoStack.length - 1];
+				const result = originalUndo.apply(this, args);
+
+				if (entry && entry.cursor && atheos && atheos.editor) {
+					const editor = atheos.editor.getActive();
+					if (editor) {
+						editor.clearSelection();
+						editor.moveCursorToPosition(entry.cursor);
+					}
+				}
+
+				return result;
+			};
+
+			UndoManager.prototype._patchedForCursor = true;
+		}
+	})();
+
+
+})();
--- settings.php
+++ settings.php
@@ -1,58 +1,57 @@
 <label><i class="fas fa-paint-brush"></i>Beautify Settings</label>
 <table>
-	<tr>
-		<td style="width: 30%;">Beautify CSS On Save</td>
-		<td style="width: 20%;">
-			<input type="checkbox" data-setting="beautify.css" class="large">
-		</td>
-		<td style="width: 30%;">Beautify HTML On Save</td>
+    <tr>
+        <td style="width: 30%;">Beautify CSS On Save</td>
+        <td style="width: 20%;">
+            <input type="checkbox" data-setting="beautify.css" class="large">
+        </td>
+        <td style="width: 30%;">Beautify HTML On Save</td>
 
-		<td style="width: 20%;">
-			<input type="checkbox" data-setting="beautify.html" class="large">
-		</td>
-	</tr>
-	<tr>
-		<td style="width: 30%;">Beautify JS On Save</td>
+        <td style="width: 20%;">
+            <input type="checkbox" data-setting="beautify.html" class="large">
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 30%;">Beautify JS On Save</td>
 
-		<td>
-			<input type="checkbox" data-setting="beautify.js" class="large">
-		</td>
-		<td style="width: 30%;">Beautify JSON On Save</td>
+        <td>
+            <input type="checkbox" data-setting="beautify.js" class="large">
+        </td>
+        <td style="width: 30%;">Beautify JSON On Save</td>
 
-		<td>
-			<input type="checkbox" data-setting="beautify.json" class="large">
-		</td>
-	</tr>
-	<tr>
-		<td style="width: 30%;">Beautify PHP On Save</td>
+        <td>
+            <input type="checkbox" data-setting="beautify.json" class="large">
+        </td>
+    </tr>
+    <tr>
+        <td style="width: 30%;">Beautify PHP On Save</td>
 
-		<td>
-			<input type="checkbox" data-setting="beautify.php" class="large">
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2">Brace Style</td>
-		<td colspan="2">
-			<select class="setting" data-setting="beautify.brace_style">
-				<option value="collapse">Collapse</option>
-				<option value="expand">Expand</option>
-				<option value="end-expand">End-Expand</option>
-				<option value="none">None</option>
-				<option value="preserve-inline">Preserve-Inline</option>
-			</select>
-		</td>
-	</tr>
-	<tr>
-		<td colspan="2" style="width: 60%;">Experimental: Guess cursor position</td>
-		<td colspan="2">
-			<toggle>
-				<input id="beautify_guessCursorPosition_true" data-setting="beautify.guessCursorPosition" value="true" name="beautify.guessCursorPosition" type="radio" />
-				<label for="beautify_guessCursorPosition_true"><?php echo i18n("enabled"); ?></label>
-				<input id="beautify_guessCursorPosition_false" data-setting="beautify.guessCursorPosition" value="false" name="beautify.guessCursorPosition" type="radio" checked />
-				<label for="beautify_guessCursorPosition_false"><?php echo i18n("disabled"); ?></label>
-			</toggle>
-		</td>
-	</tr>
+        <td>
+            <input type="checkbox" data-setting="beautify.php" class="large">
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" style="width: 60%;">Experimental: Remove trailing whitespace</td>
+        <td colspan="2">
+            <toggle>
+                <input id="beautify_removeTrailingWhitespace_true" data-setting="beautify.cleanWhitespace" value="true" name="beautify.cleanWhitespace" type="radio">
+                <label for="beautify_removeTrailingWhitespace_true"><?php echo i18n("enabled"); ?></label>
+                <input id="beautify_removeTrailingWhitespace_false" data-setting="beautify.cleanWhitespace" value="false" name="beautify.cleanWhitespace" type="radio" checked>
+                <label for="beautify_removeTrailingWhitespace_false"><?php echo i18n("disabled"); ?></label>
+            </toggle>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" style="width: 60%;">Experimental: Guess cursor position</td>
+        <td colspan="2">
+            <toggle>
+                <input id="beautify_guessCursorPosition_true" data-setting="beautify.guessCursorPosition" value="true" name="beautify.guessCursorPosition" type="radio">
+                <label for="beautify_guessCursorPosition_true"><?php echo i18n("enabled"); ?></label>
+                <input id="beautify_guessCursorPosition_false" data-setting="beautify.guessCursorPosition" value="false" name="beautify.guessCursorPosition" type="radio" checked>
+                <label for="beautify_guessCursorPosition_false"><?php echo i18n("disabled"); ?></label>
+            </toggle>
+        </td>
+    </tr>
 </table>
 
 <hint>Hint: Press Ctrl+Alt+B to Beautify</hint>
